748ea08636fc4d5771ae4c10575b6631938ca3da
古映杰 <yj_gu@ctrip.com>
2017-02-17 18:09:05 +0800

    update demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,140 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Viewport from '../../'
 import './main.css'
-
-function App() {
-    return (
-        <div className="app">
-            <div className="info" >
-                you can drag me in x-axis
-                <div style={{width:'200%'}}>adf</div>
-            </div>
-            <div className="wrapper">
-            <div className="pull_refresh">
-                        <div className="pull">
-                            <div id="arrow" className="arrow">
-                                <img src="http://alloyteam.github.io/AlloyTouch/refresh/pull_refresh/asset/arrow.png" />
-                                <br />
-                            </div>
-                        </div>
-                         <div className="loading">
-                            <svg width='40px' height='40px' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid" className="uil-default">
-                                <rect x="0" y="0" width="100" height="100" fill="none" className="bk"></rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(0 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(30 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.08333333333333333s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(60 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.16666666666666666s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(90 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.25s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(120 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.3333333333333333s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(150 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.4166666666666667s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(180 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.5s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(210 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.5833333333333334s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(240 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.6666666666666666s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(270 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.75s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(300 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.8333333333333334s' repeatCount='indefinite' />
-                                </rect><rect x='46.5' y='40' width='7' height='20' rx='5' ry='5' fill='#00a9f2' transform='rotate(330 50 50) translate(0 -30)'>
-                                    <animate attributeName='opacity' from='1' to='0' dur='1s' begin='0.9166666666666666s' repeatCount='indefinite' />
-                                </rect></svg>
-                        </div>
-                    </div>
-                <div className="list">
-                    {
-                        Array.from({length: 30}).map(
-                            (_, index) => <div className="item">{index}</div>
-                        )
-                    }
-                </div>
-                <div className="pull-up">
-                    上拉刷新还没做
-                </div>
-            </div>
-        </div>
-    )
-}
+import PullToRefresh from './pull-to-refresh'
 
 const rootElem = document.getElementById('root')
 
-ReactDOM.render(<App />, rootElem)
-
-
-const arrow = document.querySelector("#arrow")
-const pull_refresh = document.querySelector(".pull_refresh")
-const pull_up =  document.querySelector('.pull-up')
-
-const viewport = new Viewport({
-    target: '.wrapper',
-    scroller: 'body',
-    detectScroll: true,
-    onPullUp({ translateY }) {
-        pull_up.style.display = 'block'
-        pull_up.style.height = Math.min(-translateY, 40) + 'px'
-    },
-    onPullUpEnd() {
-       
-    },
-    onOrigin({ direction }) {
-        if (direction === 'bottom') {
-            pull_up.style.display = ''
-            pull_up.style.height = ''
-        }
-    },
-    onPullDown({ translateY }) {
-        if (translateY > 100) {
-            arrow.classList.add('arrow_up')
-        } else {
-            arrow.classList.remove('arrow_up')
-        }
-    },
-    async onPullDownEnd({ translateY }) {
-        if (translateY <= 100) {
-            return
-        }
-        pull_refresh.classList.add("refreshing")
-        this.preventDefault()
-        this.animateTo({x: 0, y: 40})
-        await mockRequest()
-        this.animateToOrigin()
-    },
-})
-
-function mockRequest(viewport) {
-    return new Promise(function(resolve) {
-        setTimeout(function() {
-            arrow.classList.remove("arrow_up");
-            pull_refresh.classList.remove("refreshing");
-            updateList()
-            resolve()
-        }, 1000);
-    })
+const component = {
+    'pull-to-refresh': PullToRefresh,
 }
 
-
-let count = 1
-
-function updateList() {
-    let list = document.querySelectorAll('.list .item')
-    let curCount = count++
-    Array.from(list).forEach((node, index) => node.textContent = index + curCount)
+function renderView() {
+    let App = component[location.hash] || component['pull-to-refresh']
+    ReactDOM.render(<App />, rootElem)
 }
 
-viewport.init()
-
-const viewport1 = new Viewport({
-    target: '.info',
-    // isStatic: true,
-    damp: 1,
-    detectScroll: true,
-    right: true,
-})
-
-viewport1.init()
+window.addEventListener('hashchange', renderView())
